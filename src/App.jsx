02b73ccd53b86aa741d93e2627cae75a28cb3c42
Dim <dimitri.kiavue@gmail.com>
2020-11-19 12:23:44 +0100

    [UPDATE] Get articles on App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import getHeadlinesNews from 'services/newsAPI';
 
 import 'styles/App.css';
 
 const App = () => {
-  return (
-  );
+  const [headlinesNews, setHeadlinesNews] = useState(null);
+
+  const loadHeadlines = () => {
+    getHeadlinesNews()
+    .then((response) => response.json())
+    .then((result) => {setHeadlinesNews(result.articles)});
+  }
+
+  useEffect(() => {
+    loadHeadlines();
+  }, []);
+
+  if (headlinesNews === null) {
+    return (
+      <>
+        Pas de News pour l'instant
+      </>
+    );
+  } else {
+    return (
+      <>
+        yo
+      </>
+    );
+  }
 }
 
 export default App;
--- src/services/newsAPI.jsx
+++ src/services/newsAPI.jsx
@@ -1,14 +1,9 @@
 const getHeadlinesNews = () => {
   const baseURL = process.env.REACT_APP_NEWS_API_HEADLINES_URL;
-  console.log(baseURL);
   const apiKey = process.env.REACT_APP_NEWS_API_API_KEY;
-  console.log(apiKey);
   const url = baseURL + apiKey;
-  console.log(url);
 
   return fetch(url)
-    .then((response) => response.json())
-    .then((response) => console.log(response));
 };
 
 export default getHeadlinesNews;
