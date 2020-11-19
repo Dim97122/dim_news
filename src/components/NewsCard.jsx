8cdf7390b5909f335568a888dfa0bfca7293d4a7
Dim <dimitri.kiavue@gmail.com>
2020-11-19 12:55:56 +0100

    [UPDATE] Add image on NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const NewsCard = ({ article }) => {
+  console.log(article);
   return (
     <div>
       <div>{article.title}</div>
+      <img src={article.urlToImage} />
     </div>
   );
 }
