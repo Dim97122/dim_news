ce0d0b626312c138921b03b9058ff3f15aff3a0f
Dim <dimitri.kiavue@gmail.com>
2020-11-22 14:07:56 +0100

    [UPDATE] Remove useless hooks

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import NewsCard from 'components/NewsCard';
 
 const NewsList = ({ articles }) => {
-  const [displayedArticles, setdisplayedArticles] = useState([]);
+  let displayedArticles = null;
   const articlesNumber = 4;
 
-  const selectedArticles = () => {
-    if (articles !== undefined) {
-      setdisplayedArticles(articles.slice(0, articlesNumber).map(article => <NewsCard key={article.title} article={article}/>))
-    }
+  if (articles !== undefined) {
+    displayedArticles = articles.slice(0, articlesNumber).map(article => <NewsCard key={article.title} article={article}/>)
   }
 
   return (
